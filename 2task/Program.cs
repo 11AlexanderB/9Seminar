b518f35b6f2972c19e85fefc6af18bf957499942
11AlexanderB <bezhenar93@internet.ru>
2022-09-19 23:37:49 +0300

    Сумма чисел

---
+++ 2task/Program.cs
@@ -0,0 +1,19 @@
+﻿try
+{
+    Console.Write("Введите число N: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число M: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+
+    int Numbers(int N, int M)
+    {
+        if (N > M) return 0;
+        return N + Numbers(N+1, M);
+    }
+    Console.WriteLine(Numbers(n, m));
+}
+catch
+{
+    Console.WriteLine("Ошибка");
+}
