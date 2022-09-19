80ae741690b84984a35ebf81303ba550360568d1
11AlexanderB <bezhenar93@internet.ru>
2022-09-19 23:46:44 +0300

    Функция Аккермана

---
+++ 3task/Program.cs
@@ -0,0 +1,20 @@
+﻿try
+{
+    Console.Write("Введите число N: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число M: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+
+    int Akkerman(int N, int M)
+    {
+        if (N == 0) return M + 1;
+        else if (N > 0 && M == 0) return Akkerman(N - 1, 1);
+        return Akkerman(N-1,Akkerman(N,M-1));
+    }
+    Console.WriteLine(Akkerman(n, m));
+}
+catch
+{
+    Console.WriteLine("Ошибка");
+}
