e9937b356355cf5aae119a2781dbb9b89c2fbc22
11AlexanderB <bezhenar93@internet.ru>
2022-09-19 23:33:33 +0300

    Список чисел

--- 1task/Program.cs
+++ 1task/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿try
+{
+    Console.Write("Введите число N: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число M: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+
+    string Numbers(int N, int M)
+    {
+        if (N > M) return " ";
+        return Numbers(N, M - 1) + M + " ";
+    }
+    Console.WriteLine(Numbers(n, m));
+}
+catch
+{
+    Console.WriteLine("Ошибка");
+}
